6dcf46af0a0a39f4db9dbe9bd3a98b20e7b83b7a
pkg0203 <pkg0203@naver.com>
2024-04-15 21:07:43 +0900

    accounts/signup 구현

--- spartamarket/accounts/views.py
+++ spartamarket/accounts/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.models import User
+from django.views.decorators.http import (
+    require_POST,
+    require_http_methods
+)
+from .forms import *
 
 # Create your views here.
 def login(request):
     return render(request,"accounts/login.html")
 
+@require_http_methods(["GET","POST"])
 def signup(request):
-    return render(request,"accounts/signup.html")
+    if request.method=="GET":
+        form = CustomUserCreationForm()
+        context = {
+            "form" : form,
+        }
+        return render(request,"accounts/signup.html",context)
+    else :
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid:
+            user=form.save()
+            auth_login(request,user)
+            return redirect("index")
