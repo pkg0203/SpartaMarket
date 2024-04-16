8322a9c60ed2ea1fb6811c8496612cb9e76a176e
pkg0203 <pkg0203@naver.com>
2024-04-16 20:53:38 +0900

    products : CRU 완성

--- spartamarket/products/views.py
+++ spartamarket/products/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.http import (
     require_POST,
     require_http_methods,
     )
+from django.contrib.auth.decorators import login_required
 from .models import Products
 from .forms import ProductsForm
 import ctypes
@@ -11,7 +12,7 @@ import ctypes
 @require_GET
 def show(request):
     if request.user.is_authenticated:
-        products = Products.objects.all()
+        products = Products.objects.all().order_by('-pk')
         context={
             "products" : products
         }
@@ -19,7 +20,8 @@ def show(request):
     else :
         ctypes.windll.user32.MessageBoxW(0, "로그인이 필요합니다!", "Error", 16)
         return redirect("accounts:login")
-    
+
+@require_http_methods(["GET", "POST"])
 def create(request):
     if request.method=="GET":
         form = ProductsForm()
@@ -43,11 +45,20 @@ def detail(request,pk):
     }
     return render(request,"products/detail.html",context)
 
+
 @require_http_methods(["GET", "POST"])
 def update(request,pk):
+    product = get_object_or_404(Products,pk=pk)
     if request.method=="GET":
-        product = get_object_or_404(Products,pk=pk)
+        form=ProductsForm(instance=product)
         context={
-            'product' : ProductsForm(instance=product)
+            'form' : form,
+            'product' : product,
         }
         return render(request,"products/update.html",context)
+    
+    elif request.method=="POST":
+        form= ProductsForm(request.POST,instance=product)
+        if form.is_valid():
+            product=form.save()
+        return redirect('products:detail',product.pk)
