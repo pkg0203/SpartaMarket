7cfaccfb11d8fa9a75422f04b1a2236af4cfefe0
pkg0203 <pkg0203@naver.com>
2024-04-16 16:49:35 +0900

    products/show : login이 아닌 상태면 login page로 보냄

--- spartamarket/products/views.py
+++ spartamarket/products/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from . import views
+from django.shortcuts import render,redirect
+import ctypes
 
 # Create your views here.
 def show(request):
-    return render(request,"products/show.html")
+    if request.user.is_authenticated:
+        return render(request,"products/show.html")
+    else :
+        ctypes.windll.user32.MessageBoxW(0, "로그인이 필요합니다!", "Error", 16)
+        return redirect("accounts:login")
