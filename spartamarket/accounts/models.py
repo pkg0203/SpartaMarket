587b77b2c291332f561e3b7da2dad5b0b6f38657
pkg0203 <pkg0203@naver.com>
2024-04-17 15:21:09 +0900

    accounts/models : follow field 추가

--- spartamarket/accounts/models.py
+++ spartamarket/accounts/models.py
@@ -7,4 +7,13 @@ class User(AbstractUser):
         MALE = 'M', '남성'
         FEMALE = 'F', '여성'
     age = models.IntegerField()
-    gender = models.CharField(choices=GenderChoices.choices, max_length=1, blank=False)
+    gender = models.CharField(
+        choices=GenderChoices.choices, 
+        max_length=1,
+        blank=False
+    )
+    follow = models.ManyToManyField(
+        'self',
+        related_name='followers',
+        symmetrical=False
+    )
