a72e82b02abfaf7641a7e5da9d5ac0f26f5bbf3a
pkg0203 <pkg0203@naver.com>
2024-04-18 16:36:28 +0900

    해시태그 이제 게시글마다 보임

--- spartamarket/accounts/urls.py
+++ spartamarket/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = "accounts"
 urlpatterns = [
     path('login/', views.login, name='login'),
@@ -9,3 +12,8 @@ urlpatterns = [
     path('<int:pk>/mypage/', views.mypage, name='mypage'),
     path('<int:pk>/follow/', views.follow, name='follow'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                        document_root=settings.MEDIA_ROOT)
--- spartamarket/products/urls.py
+++ spartamarket/products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = "products"
 urlpatterns = [
     path('show/', views.show, name='show'),
@@ -17,3 +20,6 @@ urlpatterns = [
 
     path('<int:pk>/jjim/', views.jjim, name='jjim'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
